de176110ca9c89028b09bfd023080a7f4307d0ae
Quy3n <quyenngoc19102002@gmail.com>
2025-02-18 17:10:22 +0700

    update exercise 1->6 Collection

--- src/collection/ProductServiceDemo.java
+++ src/collection/ProductServiceDemo.java
@@ -12,8 +12,16 @@ public class ProductServiceDemo {
         Product meat = new Product("Meat", 5, "Food", 1);
 
         //1. Lưu danh sách sản phẩm, thêm, xoá và hiển thị danh sách sản phẩm.
-        ArrayList<Product> products = (ArrayList<Product>) Arrays.asList(hamburger, lemon, bread, meat);
+        ArrayList<Product> products = new ArrayList<>();
         products.add(hamburger);
+        products.add(hamburger);
+        products.add(lemon);
+        products.add(2,meat);
+        products.add(bread);
+        products.add(bread);
+        products.add(lemon);
+        products.add(lemon);
+        products.add(lemon);
         products.add(new Product("Pizza", 6, "Fast Food", 1));
         products.remove(lemon);
         System.out.println("Danh sách sản phẩm:");
@@ -39,7 +47,36 @@ public class ProductServiceDemo {
 
         //6. Dùng Hash set Loại bỏ sản phẩm trùng lặp trong danh sách
         HashSet<Product> setProduct = new HashSet<>(products);
+        System.out.println("Danh sách sản phẩm (không bị trùng lặp):");
         System.out.println(setProduct);
+
+        //7. Dùng HashSet<User> để quản lý danh sách khách hàng đăng ký. Kiểm tra một email đã tồn tại trong danh sách chưa.
+
+        //8. Lưu sản phẩm theo danh mục bảng chữ cái
+
+        //9. Dùng LinkedHashSet<Order> để lưu các đơn hàng đã hoàn thành theo thứ tự đặt hàng.
+
+        //10. Sử dụng Set<Product> để lưu danh sách yêu thích của khách hàng. Check 1 sản phẩm có trong ds hay không
+
+        //11. Dùng HashMap<String, Integer> để lưu số lượng sản phẩm theo mã sản phẩm (productID).
+
+        //12. Dùng HashMap<String, Integer> để đếm số lần khách hàng đã mua hàng.
+
+        //13. Sử dụng HashMap<Product, Integer> để lưu giỏ hàng với sản phẩm và số lượng.
+
+        //14. Dùng HashMap<User, List<Review>> để quản lý danh sách đánh giá của từng người dùng.
+
+        //15. Sử dụng TreeMap<Double, List<Product>> để lưu sản phẩm theo giá, giúp lấy danh sách sản phẩm theo khoảng giá nhanh hơn.
+
+        //16. Sử dụng Map<Order, List<Product>> để lưu danh sách sản phẩm của mỗi đơn hàng.
+
+        //17. Dùng Map<User, Integer> để lưu số lần mua hàng của mỗi khách hàng, viết phương thức tìm khách hàng có số đơn hàng nhiều nhất.
+
+        //18. Dùng HashMap<Product, Integer> để đếm số lần sản phẩm được đặt hàng. Trả về danh sách 5 sản phẩm được mua nhiều nhất.
+
+        //19. Dùng Map<String, Set<String>> để lưu danh mục con của mỗi danh mục cha. Ví dụ: {"Điện tử" -> {"Điện thoại", "Laptop"}}.
+
+        //20. Dùng LinkedList<String> để lưu lịch sử tìm kiếm sản phẩm. Chỉ giữ tối đa 10 tìm kiếm gần nhất, nếu vượt quá thì xoá phần tử cũ nhất.
 
     }
 
