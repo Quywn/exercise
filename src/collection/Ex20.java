6734603f0267c8a672fc359997e4faea7351052d
Quy3n <quyenngoc19102002@gmail.com>
2025-02-24 17:37:35 +0700

    sửa logic ex20 bài tập collection theo đúng yêu cầu

--- src/collection/Ex20.java
+++ src/collection/Ex20.java
@@ -2,21 +2,26 @@ package collection;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Ex20 {
     public static final int MAX_SIZE = 10;
     public static void main(String[] args) {
         //Ex20. Dùng LinkedList<String> để lưu lịch sử tìm kiếm sản phẩm. Chỉ giữ tối đa 10 tìm kiếm gần nhất, nếu vượt quá thì xoá phần tử cũ nhất.
-        List<String> productList = new ArrayList<>(Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J"));
+        LinkedList<String> productList = new LinkedList<>(Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J"));
         String newSearch = "newSearch";
-        productList.addFirst(newSearch);
-        if (productList.size() >= MAX_SIZE) {
+        if (isOverdose(productList)) {
             productList.removeLast();
+            productList.addFirst(newSearch);
+        } else {
+            productList.add(newSearch);
         }
-
         System.out.println(productList);
+    }
 
+    static boolean isOverdose(List<String> productList) {
+        return productList.size() > MAX_SIZE;
     }
 
 }
